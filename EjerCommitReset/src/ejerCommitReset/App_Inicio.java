7832bd2f74167d0daad62cd977f9ab1e5b412ddf
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-05-18 20:28:32 +0200

    Primer commit

---
+++ EjerCommitReset/src/ejerCommitReset/App_Inicio.java
@@ -0,0 +1,10 @@
+package ejerCommitReset;
+
+public class App_Inicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer commit");
+	}
+
+}
