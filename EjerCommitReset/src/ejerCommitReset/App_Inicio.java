cd160ddf951078e62ea4f978759199b97c16f6f7
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-05-18 20:31:14 +0200

    segundo commit

--- EjerCommitReset/src/ejerCommitReset/App_Inicio.java
+++ EjerCommitReset/src/ejerCommitReset/App_Inicio.java
@@ -5,6 +5,7 @@ public class App_Inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer commit");
+		System.out.println("18/05/2020 - segundo commit");
 	}
 
 }
